641b2488d07af4cfcbdee21c3839059d405f4edf
JeonYekyeong <jeonyk219@gmail.com>
2024-04-18 22:04:12 +0900

    회원가입 중복 체크

--- src/main/java/com/yekj/proejct/chatapplication/repository/UserRepository.java
+++ src/main/java/com/yekj/proejct/chatapplication/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.yekj.proejct.chatapplication.repository;
 import com.yekj.proejct.chatapplication.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/com/yekj/proejct/chatapplication/service/UserService.java
+++ src/main/java/com/yekj/proejct/chatapplication/service/UserService.java
@@ -3,17 +3,31 @@ package com.yekj.proejct.chatapplication.service;
 import com.yekj.proejct.chatapplication.model.User;
 import com.yekj.proejct.chatapplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public User addUser(User user) {
+    public User addUser(User user) throws Exception{
         user.setUserId(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Optional<User> findUser = repository.findByUsername(user.getUsername());
+
+        if(findUser.isPresent()) {
+            // 중복된 사용자 이름은 사용 불가능
+            throw new Exception("중복된 사용자 이름입니다.");
+        }
+
         return repository.save(user);
     }
 }
